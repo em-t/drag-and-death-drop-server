6a31ac8fd784444b24776bc26032cfe6d8bb80bf
kiertorata <eva.taivalsaari@gmail.com>
2018-11-15 16:08:49 +0200

    basic get functionalities done

--- src/main/java/net/emt/demo/DragController.java
+++ src/main/java/net/emt/demo/DragController.java
@@ -1,33 +1,41 @@
 package net.emt.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class DragController {
-    private QueenDAO dao;
+    private QueenDAO queenDAO;
+    private FactDAO factDAO;
 
     @Autowired
-    public DragController(QueenDAO dao) {
-        this.dao = dao;
+    public DragController(QueenDAO queenDAO, FactDAO factDAO) {
+        this.queenDAO = queenDAO;
+        this.factDAO = factDAO;
     }
 
     @RequestMapping(value = "/api/queens", method = RequestMethod.GET)
     public List<Queen> getQueens() {
-        List<Queen> queens = dao.listAllQueens();
+        List<Queen> queens = queenDAO.listAllQueens();
         return queens;
     }
 
-    // TODO: metodi joka palauttaa random queenin
     @RequestMapping(value = "/api/randomQueen", method = RequestMethod.GET)
     public Queen getOneRandomQueen() {
-        Queen queen = dao.oneRandomQueen();
+        Queen queen = queenDAO.oneRandomQueen();
         return queen;
     }
 
-    // TODO: metodi joka palauttaa queenin faktat oikealla id:llä
+    @RequestMapping(value = "/api/factsOfQueen/{id}", method = RequestMethod.GET)
+    public List<Fact> getFactsofQueenWithId(@PathVariable("id") int id, @RequestParam(value="amount", required = false) Integer amount) {
+        List<Fact> facts;
+        if (amount == null) {
+            facts = factDAO.getFactsForQueen(id);
+            return facts;
+        }
+        facts = factDAO.getFactsForQueen(id, amount);
+        return facts;
+    }
 }
--- src/main/java/net/emt/demo/FactDAO.java
+++ src/main/java/net/emt/demo/FactDAO.java
@@ -3,9 +3,11 @@ package net.emt.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Service;
 
 import java.sql.ResultSet;
+import java.util.List;
 
 @Service
 public class FactDAO {
@@ -27,5 +29,22 @@ public class FactDAO {
         this.jdbc = jdbc;
     }
 
+    public List<Fact> getFactsForQueen(int queenId, int numberOfFactsWanted) {
+        String sql = "SELECT * FROM fact WHERE queen_id=? ORDER BY random() LIMIT ?";
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+        parameters.addValue("queenId", queenId);
+        parameters.addValue("limit", numberOfFactsWanted);
 
+        List<Fact> facts = jdbc.query(sql, mapper, queenId, numberOfFactsWanted);
+        return facts;
+    }
+
+    public List<Fact> getFactsForQueen(int queenId) {
+        String sql = "SELECT * FROM fact WHERE queen_id=? ORDER BY random()";
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+        parameters.addValue("queenId", queenId);
+
+        List<Fact> facts = jdbc.query(sql, mapper, queenId);
+        return facts;
+    }
 }
